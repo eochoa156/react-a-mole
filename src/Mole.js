541f23515fc6185c2f8ee8bbfb4af033d49aca0e
Emily Ochoa <eochoa156@gmail.com>
2024-04-04 07:45:18 -0700

    added mole and mole container files

---
+++ src/Mole.js
@@ -0,0 +1,9 @@
+function Mole () {
+    return (
+        <div>
+            <p>Mole.js file</p>
+        </div>
+    )
+}
+
+export default Mole
---
+++ src/MoleContainer.js
@@ -0,0 +1,12 @@
+import Mole from "./Mole";
+
+function MoleContainer (props) {
+    return (
+        <div>
+            <h2>Mole Container</h2>
+            <Mole />
+        </div>
+    )
+}
+
+export default MoleContainer
